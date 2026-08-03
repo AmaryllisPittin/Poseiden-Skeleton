1805f42733ea44d72ac33f3e2ce13552de43d508
Amaryllis Pittin <amaryllispittin@gmail.com>
2026-08-03 12:59:22 +0200

    Dépendances et premier commit

---
+++ src/main/java/com/nnk/springboot/domain/Rating.java
@@ -0,0 +1,9 @@
+package com.nnk.springboot.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rating")
+public class Rating {
+    // TODO: Map columns in data table RATING with corresponding java fields
+}
---
+++ src/main/java/com/nnk/springboot/domain/Trade.java
@@ -0,0 +1,9 @@
+package com.nnk.springboot.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "trade")
+public class Trade {
+    // TODO: Map columns in data table TRADE with corresponding java fields
+}
